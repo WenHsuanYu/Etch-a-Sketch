3fa8341fe954c40c3398d43a3f45f12b717ece07
WenHsuanYu <asdfjoeasdfjoe@gmail.com>
2023-08-17 12:49:07 +0800

    Add the adjustable number of grid square feature

--- grid.js
+++ grid.js
@@ -2,6 +2,10 @@ let gridSize = document.querySelector("input[type=range]");
 const grid = document.querySelector(".grid");
 let gridNumber = document.querySelector(".gridsize");
 let colorPicker = document.querySelector("input[type=color]");
+let normalButton = document.querySelector("#normalbtn");
+let rainbowButton = document.querySelector("#rainbowbtn");
+let eraserButton = document.querySelector("#eraserbtn");
+let clearButton = document.querySelector("#clearbtn");
 let NumberOfgirdSize = 16;
 
 createGrid(NumberOfgirdSize);
@@ -9,8 +13,6 @@ createGrid(NumberOfgirdSize);
 function createDiv(size) {
     const div = document.createElement("div");
     div.classList.add("grid-box");
-    div.style.boxSizing = "border-box";
-    div.style.border = "1px solid";
     div.style.width = `${size}px`;
     div.style.height = `${size}px`;
     return div;
@@ -21,4 +23,16 @@ function createGrid(gridSize) {
         grid.appendChild(createDiv(grid.clientWidth / gridSize));
     }
 }
+
+clearButton.addEventListener("click", clearGrid);
+
+function clearGrid() {
+    grid.textContent = "";
+}
+
+gridSize.addEventListener("input", () => {
+    gridNumber.textContent = `${gridSize.value} x ${gridSize.value}`;
+    grid.textContent = "";
+    createGrid(gridSize.value);    
+});
 
